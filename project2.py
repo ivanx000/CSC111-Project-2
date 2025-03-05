ca80a441a1512b6bf41ec1b7f24a0d51e65933d4
ivanx000 <ivanxie102@gmail.com>
2025-03-05 14:33:48 -0500

    starting code

--- project2.py
+++ project2.py
@@ -1,1 +1,186 @@
+"""CSC111 Project 2"""
+from __future__ import annotations
 
+import csv
+from typing import Any, Optional
+
+from python_ta.contracts import check_contracts
+
+
+# @check_contracts - We are commenting this out, so it doesn't slow down the code for Part 1.2
+class Tree:
+    """A recursive tree data structure.
+
+    Note the relationship between this class and RecursiveList; the only major
+    difference is that _rest has been replaced by _subtrees to handle multiple
+    recursive sub-parts.
+
+    Representation Invariants:
+        - self._root is not None or self._subtrees == []
+        - all(not subtree.is_empty() for subtree in self._subtrees)
+    """
+    # Private Instance Attributes:
+    #   - _root:
+    #       The item stored at this tree's root, or None if the tree is empty.
+    #   - _subtrees:
+    #       The list of subtrees of this tree. This attribute is empty when
+    #       self._root is None (representing an empty tree). However, this attribute
+    #       may be empty when self._root is not None, which represents a tree consisting
+    #       of just one item.
+    _root: Optional[Any]
+    _subtrees: list[Tree]
+
+    def __init__(self, root: Optional[Any], subtrees: list[Tree]) -> None:
+        """Initialize a new Tree with the given root value and subtrees.
+
+        If root is None, the tree is empty.
+
+        Preconditions:
+            - root is not none or subtrees == []
+        """
+        self._root = root
+        self._subtrees = subtrees
+
+    def is_empty(self) -> bool:
+        """Return whether this tree is empty.
+
+        >>> t1 = Tree(None, [])
+        >>> t1.is_empty()
+        True
+        >>> t2 = Tree(3, [])
+        >>> t2.is_empty()
+        False
+        """
+        return self._root is None
+
+    def __len__(self) -> int:
+        """Return the number of items contained in this tree.
+
+        >>> t1 = Tree(None, [])
+        >>> len(t1)
+        0
+        >>> t2 = Tree(3, [Tree(4, []), Tree(1, [])])
+        >>> len(t2)
+        3
+        """
+        if self.is_empty():
+            return 0
+        else:
+            size = 1  # count the root
+            for subtree in self._subtrees:
+                size += subtree.__len__()  # could also write len(subtree)
+            return size
+
+    def __contains__(self, item: Any) -> bool:
+        """Return whether the given is in this tree.
+
+        >>> t = Tree(1, [Tree(2, []), Tree(5, [])])
+        >>> t.__contains__(1)
+        True
+        >>> t.__contains__(5)
+        True
+        >>> t.__contains__(4)
+        False
+        """
+        if self.is_empty():
+            return False
+        elif self._root == item:
+            return True
+        else:
+            for subtree in self._subtrees:
+                if subtree.__contains__(item):
+                    return True
+            return False
+
+    def __str__(self) -> str:
+        """Return a string representation of this tree.
+
+        For each node, its item is printed before any of its
+        descendants' items. The output is nicely indented.
+
+        You may find this method helpful for debugging.
+        """
+        return self._str_indented(0).rstrip()
+
+    def _str_indented(self, depth: int) -> str:
+        """Return an indented string representation of this tree.
+
+        The indentation level is specified by the <depth> parameter.
+        """
+        if self.is_empty():
+            return ''
+        else:
+            str_so_far = '  ' * depth + f'{self._root}\n'
+            for subtree in self._subtrees:
+                # Note that the 'depth' argument to the recursive call is
+                # modified.
+                str_so_far += subtree._str_indented(depth + 1)
+            return str_so_far
+
+    def remove(self, item: Any) -> bool:
+        """Delete *one* occurrence of the given item from this tree.
+
+        Do nothing if the item is not in this tree.
+        Return whether the given item was deleted.
+        """
+        if self.is_empty():
+            return False
+        elif self._root == item:
+            self._delete_root()  # delete the root
+            return True
+        else:
+            for subtree in self._subtrees:
+                deleted = subtree.remove(item)
+                if deleted and subtree.is_empty():
+                    # The item was deleted and the subtree is now empty.
+                    # We should remove the subtree from the list of subtrees.
+                    # Note that mutate a list while looping through it is
+                    # EXTREMELY DANGEROUS!
+                    # We are only doing it because we return immediately
+                    # afterward, and so no more loop iterations occur.
+                    self._subtrees.remove(subtree)
+                    return True
+                elif deleted:
+                    # The item was deleted, and the subtree is not empty.
+                    return True
+
+            # If the loop doesn't return early, the item was not deleted from
+            # any of the subtrees. In this case, the item does not appear
+            # in this tree.
+            return False
+
+    def _delete_root(self) -> None:
+        """Remove the root item of this tree.
+
+        Preconditions:
+            - not self.is_empty()
+        """
+        if not self._subtrees:
+            self._root = None
+        else:
+            # Strategy: Promote a subtree (the rightmost one is chosen here).
+            # Get the last subtree in this tree.
+            last_subtree = self._subtrees.pop()
+
+            self._root = last_subtree._root
+            self._subtrees.extend(last_subtree._subtrees)
+
+
+@check_contracts
+def build_decision_tree(file: str) -> Tree:
+    """Build a decision tree storing the animal data from the given file.
+
+    Preconditions:
+        - file is the path to a csv file in the format of the provided animals.csv
+    """
+    tree = Tree('', [])  # The start of a decision tree
+
+    with open(file) as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)  # skip the header row
+
+        for row in reader:
+            ...
+            # tree.insert_sequence(row)
+
+    return tree
